662ba99e39a4065c50eab784f73863d8a01f062a
Stef Spears <spears.sj87@gmail.com>
2019-10-17 03:21:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,8 +13,9 @@ class Scraper
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
-      binding.pry
+      
     end
+    binding.pry
   end 
   
   
